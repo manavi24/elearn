87760bf912ec91c410b39dcd8889d7af2ba42bd6
manavi <manavi.singh.2001@gmail.com>
2024-08-14 16:16:17 +0530

    Added frontend and backend repositories

---
+++ frontend/src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { UserContextProvider } from './context/UserContext';
+import { CourseContextProvider } from './context/CourseContext';
+
+export const server = "https://elearning-server-1-d04a.onrender.com";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <UserContextProvider>
+      <CourseContextProvider>
+        <App />
+      </CourseContextProvider>
+    </UserContextProvider>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
